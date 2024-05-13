ccd2d18edd20629632b0f0fd20a66843a7ff4855
Polpath.S <polpath.s@ku.th>
2024-05-13 13:01:45 +0700

    add controller

---
+++ Controllers/TestController.cs
@@ -0,0 +1,21 @@
+﻿using be.Model;
+using Microsoft.AspNetCore.Mvc;
+
+namespace be.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class TestController : ControllerBase
+    {
+        public TestController()
+        {
+
+        }
+
+        [HttpGet(Name = "Test")]
+        public bool Get()
+        {
+            return true;
+        }
+    }
+}
